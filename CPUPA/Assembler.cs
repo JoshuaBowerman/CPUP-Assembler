b3a100c2aea8a6040b82581116a2ae99b3e4e076
Joshua Bowerman <JoshuaBowerman@users.noreply.github.com>
2021-01-29 17:22:46 -0500

    Docs

---
+++ CPUPA/Assembler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPUPA
+{
+    class Assembler
+    {
+
+        public Assembler()
+        {
+
+        }
+
+        public addFile(string[] file)
+        {
+
+        }
+    }
+}
--- CPUPA/Program.cs
+++ CPUPA/Program.cs
@@ -4,9 +4,23 @@ namespace CPUPA
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("CPUP Assembler");
+                Console.WriteLine("Accepts a CPA file and outputs a machine code file either in bin or MIF formats.");
+                Console.WriteLine("Usage: CPUPA.exe [Swtiches] Input.cpa output.bin ");
+                Console.WriteLine("Switches:");
+                Console.WriteLine("-M : Output file in MIF format");
+                Console.WriteLine("-B : Output file in bin format");
+                Console.WriteLine("");
+                return 0;
+            }
+
+
+
+            return 0;
         }
     }
 }
